d1d0fc016b788d8adb2fc4a34af03335e01b677e
chabchab <jiyoung6301@gmail.com>
2024-01-12 01:16:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/listviewpractice2/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.listviewpractice2
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.listviewpractice2.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+    }
+}
