266a51ac21e438f2d21a948a3f238fbd51a9366a
chabchab <jiyoung6301@gmail.com>
2024-01-12 01:43:52 +0900

    adapter 뼈대작업 완료

--- app/src/main/java/com/example/listviewpractice2/adpaters/StudentAdapter.kt
+++ app/src/main/java/com/example/listviewpractice2/adpaters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.listviewpractice2.adpaters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listviewpractice2.R
 import com.example.listviewpractice2.datas.Student
 
 //어떤 종류의 데이터를 받아서 화면에 뿌려줄지? Student데이터를 뿌릴 거이므로!
@@ -9,8 +13,19 @@ class StudentAdapter(val mContext:Context,
                      val resID:Int,
                      val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resID, mList){
 
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+
+        }
+        val row = tempRow!!
+        return row
+    }
 
 
 
-
-}A
+}
